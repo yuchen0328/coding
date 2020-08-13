a878cf4fe0a0c770baba1fd0705214a7fac463d2
yuchen0328 <69499308+yuchen0328@users.noreply.github.com>
2020-08-13 14:48:11 +0800

    Add files via upload

--- eighth.py
+++ eighth.py
@@ -5,3 +5,15 @@ Created on Thu Aug 13 14:26:06 2020
 @author: USER
 """
 
+from mcpi.minecraft import Minecraft
+import time
+#import random
+
+mc=Minecraft.create()
+
+x,y,z = mc.player.getTilePos()
+while True:
+    mc.executeCommand("weather clear")
+    time.sleep(5)
+    mc.executeCommand("weather rain")
+    time.sleep(5)
