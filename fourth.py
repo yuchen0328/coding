c0296369e2f3715bc993989f6dc5e1cab9458361
yuchen0328 <69499308+yuchen0328@users.noreply.github.com>
2020-08-14 15:54:45 +0800

    Add files via upload

--- fourth.py
+++ fourth.py
@@ -4,26 +4,22 @@ Created on Wed Aug 12 16:12:52 2020
 
 @author: USER
 """
-
 from mcpi.minecraft import Minecraft
-import buildPyramid
-#import time
-#import random
 mc=Minecraft.create()
+import random
+x,y,z = mc.player.getTilePos() 
 
-x,y,z = mc.player.getPos()
-
-def buildPyram(x,y,z,base):
+def buildPyramid(x,y,z,base):
     height = (base//2)+1
     for i in range(height):
-        x1 = x + i
-        y1 = y + i
-        z1 = z + i
+        x1=x+i
+        y1=y+i
+        z1=z+i
         
-        x2 = x + base - i
-        
-        z2 = z + base - i
-        mc.setBlock(x1.y1,z1,x2,y,z2,24)
-
-x,y,z = mc.player.getPos()    
+        x2=x+base-i
+        z2=z+base-i
+        r = random.randrange(0,15)
+        mc.setBlocks(x1,y1,z1,x2,y,z2,251,r)
+x,y,z = mc.player.getTilePos()    
 buildPyramid(x,y,z,20)
+#金字塔
